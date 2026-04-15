0209ed733773b1b37a9b26b35f087ac8f6a0ed11
1NTROO <95148848+1NTROO@users.noreply.github.com>
2026-04-15 12:44:46 +0200

    chore: code cleanup

--- PacManButSlavery/Assets/Scripts/PlayerMovement.cs
+++ PacManButSlavery/Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine.UIElements;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
-    private float currentSpeed;
+    private Vector3 currentSpeed;
 
     private Vector2 movementInput;
     private float moveDirection;
@@ -18,14 +18,18 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         GetInput();
-        transform.position += new Vector3(moveDirection < 0 ? movementInput.x : 0, moveDirection > 0 ? movementInput.y : 0) * speed * Time.deltaTime;
+
+        currentSpeed = new Vector3( moveDirection < 0 ? movementInput.x : 0, 
+                                    moveDirection > 0 ? movementInput.y : 0) 
+                                    * speed 
+                                    * Time.deltaTime;
+        
+        transform.position += currentSpeed;
     }
 
     void GetInput()
     {
         movementInput = InputSystem.actions["move"].ReadValue<Vector2>();
-        print(movementInput);
         moveDirection = InputSystem.actions["movedirection"].ReadValue<float>();
-        print(moveDirection);
     }
 }
